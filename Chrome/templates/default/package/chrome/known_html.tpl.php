f4bdddc721de851d60123dbbc8f09a6f6dd89293
Marcus Povey <marcus@marcus-povey.co.uk>
2014-12-08 19:34:55 +0000

    Hopefully adapt size better

--- Chrome/templates/default/package/chrome/known_html.tpl.php
+++ Chrome/templates/default/package/chrome/known_html.tpl.php
@@ -8,17 +8,15 @@
 		overflow-x: hidden;
 	    }
 
-	    img {
-		margin: 5px;
-		border: 2px solid black;
-		vertical-align: middle;
-		width: 75px;
-		height: 75px;
+	    iframe {
+		min-width: 357px;
 	    }
 	</style>
 	<script src="known.js"></script>
     </head>
     <body>
-	<iframe id="iframe" src="<?= \Idno\Core\site()->config()->getDisplayURL(); ?>chrome/share" style="border: 0; width: 100%; height: 100%" frameborder="0" scrolling="no" >Your browser doesn't support iframes</iframe>
+	<div id="content">
+	    <iframe id="iframe" src="<?= \Idno\Core\site()->config()->getDisplayURL(); ?>chrome/share" style="border: 0; width: 100%; height: 100%" frameborder="0" scrolling="no" >Your browser doesn't support iframes</iframe>
+	</div>
     </body>
 </html>
