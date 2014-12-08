e6d3df3bd7681cefc3cd78ab354ba62f4fcdc11d
Marcus Povey <marcus@marcus-povey.co.uk>
2014-12-08 20:16:17 +0000

    Attempting to create the object dynamically

--- Chrome/templates/default/package/chrome/known_html.tpl.php
+++ Chrome/templates/default/package/chrome/known_html.tpl.php
@@ -15,6 +15,6 @@
 	<script src="known.js"></script>
     </head>
     <body>
-	    <iframe id="iframe" src="<?= \Idno\Core\site()->config()->getDisplayURL(); ?>share" style="border: 0; width: 100%; height: 100%" frameborder="0" scrolling="no">Your browser doesn't support iframes</iframe>
+	    <!-- <iframe id="iframe" src="<?= \Idno\Core\site()->config()->getDisplayURL(); ?>share" style="border: 0; width: 100%; height: 100%" frameborder="0" scrolling="no">Your browser doesn't support iframes</iframe> -->
     </body>
 </html>
--- Chrome/templates/default/package/chrome/known_js.tpl.php
+++ Chrome/templates/default/package/chrome/known_js.tpl.php
@@ -4,11 +4,30 @@ function resizeIframe() {
     obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
 }
 
-chrome.tabs.getSelected(null, function(tab) {
-    document.getElementById('iframe').src = '<?= \Idno\Core\site()->config()->getDisplayURL(); ?>share?via=ff_social&share_url=' + encodeURIComponent(tab.url) +'&share_title=' + encodeURIComponent(tab.title);
-});
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById("iframe").addEventListener('load', resizeIframe);
+
+    var frame = document.createElement('iframe');
+    frame.setAttribute('width', '100%');
+    frame.setAttribute('height', '100%');
+    frame.setAttribute('frameborder', '0');
+    frame.setAttribute('id', 'iframe');
+
+    frame.setAttribute('src', 'http://www.rememberthemilk.com/services/modules/googleig/');
+
+    document.body.appendChild(frame);
+
+    
+    
+    frame.addEventListener('load', resizeIframe);
+    //document.getElementById("iframe").addEventListener('load', resizeIframe);
+    
+    
+    
+    
+    chrome.tabs.getSelected(null, function(tab) {
+	document.getElementById('iframe').src = '<?= \Idno\Core\site()->config()->getDisplayURL(); ?>share?via=ff_social&share_url=' + encodeURIComponent(tab.url) +'&share_title=' + encodeURIComponent(tab.title);
+    });
+
 });
