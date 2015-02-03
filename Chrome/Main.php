ff2c4204b2e87a7bec3e7e0047fff4e15dfa6cd8
Marcus Povey <marcus@marcus-povey.co.uk>
2015-02-03 13:20:31 +0000

    DER/PEM conversion from https://php.net/manual/en/ref.openssl.php#74188

--- Chrome/Main.php
+++ Chrome/Main.php
@@ -1,21 +1,38 @@
 <?php
 
-    namespace IdnoPlugins\Chrome {
+namespace IdnoPlugins\Chrome {
 
-        class Main extends \Idno\Common\Plugin {
+    class Main extends \Idno\Common\Plugin {
 
-            function registerPages() {
-                // Register settings page
-                \Idno\Core\site()->addPageHandler('account/chrome','\IdnoPlugins\Chrome\Pages\Account');
+	function registerPages() {
+	    // Register settings page
+	    \Idno\Core\site()->addPageHandler('account/chrome', '\IdnoPlugins\Chrome\Pages\Account');
 
-                // Handlers
-		\Idno\Core\site()->addPageHandler('chrome/download/?','\IdnoPlugins\Chrome\Pages\Download'); 
+	    // Handlers
+	    \Idno\Core\site()->addPageHandler('chrome/download/?', '\IdnoPlugins\Chrome\Pages\Download');
 
-                /** Template extensions */
-                // Add menu items to account screen
-                \Idno\Core\site()->template()->extendTemplate('account/menu/items','account/chrome/menu');
-            }
+	    /** Template extensions */
+	    // Add menu items to account screen
+	    \Idno\Core\site()->template()->extendTemplate('account/menu/items', 'account/chrome/menu');
+	}
 
-        }
+	/** DER/PEM conversion from https://php.net/manual/en/ref.openssl.php#74188 */
+	static function pem2der($pem_data) {
+	    $begin = "CERTIFICATE-----";
+	    $end = "-----END";
+	    $pem_data = substr($pem_data, strpos($pem_data, $begin) + strlen($begin));
+	    $pem_data = substr($pem_data, 0, strpos($pem_data, $end));
+	    $der = base64_decode($pem_data);
+	    return $der;
+	}
 
+	static function der2pem($der_data) {
+	    $pem = chunk_split(base64_encode($der_data), 64, "\n");
+	    $pem = "-----BEGIN CERTIFICATE-----\n" . $pem . "-----END CERTIFICATE-----\n";
+	    return $pem;
+	}
+
+	/*	 * **** */
     }
+
+}
