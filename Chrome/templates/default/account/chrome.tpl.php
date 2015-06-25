faae48d3e3d857fed3ae16292b7e7f4607689367
Marcus Povey <marcus@marcus-povey.co.uk>
2015-06-25 13:36:50 +0100

    Bootstrap 3 updates

--- Chrome/templates/default/account/chrome.tpl.php
+++ Chrome/templates/default/account/chrome.tpl.php
@@ -1,13 +1,13 @@
 
 <div class="row">
 
-    <div class="span10 offset1">
+    <div class="col-md-10 col-md-offset-1">
         <h1>Chrome</h1>
         <?= $this->draw('account/menu') ?>
     </div>
 </div>
 <div class="row">
-    <div class="span10 offset1" style="margin-top: 4em">
+    <div class="col-md-10 col-md-offset-1" style="margin-top: 4em">
 	<p>
             If you're using Google Chrome (or the open source Chromium browser), click the button below to
             download an application for your site!
